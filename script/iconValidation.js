6e4ccb3d7c12b7e3ec362a5ea313798b4d319b35
Feppeli <dfsilvat@gmail.com>
2022-03-24 23:54:50 -0300

    fix validation bugs

--- script/iconValidation.js
+++ script/iconValidation.js
@@ -4,10 +4,10 @@ button1.addEventListener('click', function() {
     var icon = document.querySelector('#i1')
     var inputFN = document.querySelector('#inputFirstName')
     
-    if (inputFN.value === ""){
+    if(inputFN.value == ""){
         icon.style.opacity = "100%"
-    }else if(inputFN.value !== ""){
-        icon.style.display = "0%"
+    }else{
+        icon.style.opacity = "0%"
     }
 })
 
--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 document.querySelector('#submit').addEventListener('click', function(e){
+    var espaco1 = document.querySelector('.errormsgFName')
+
     var inputFirstName = document.querySelector('#inputFirstName')
     var firstName = inputFirstName.value
     const msgErrorFirstName = `<p style="color: red; margin: 0;text-align: right;">First Name cannot be empty</p>`
     const labelFirstName =document.querySelector('.errormsgFName')
-    if(firstName == ""){
+    if(firstName == "" && espaco1.childNodes.length == 0){
         let elementFirstName = document.createElement('label')
         elementFirstName.innerHTML = msgErrorFirstName
         labelFirstName.appendChild(elementFirstName)
@@ -16,12 +18,14 @@ document.querySelector('#submit').addEventListener('click', function(e){
 });
 
 document.querySelector('#submit').addEventListener('click', function() {
+    var espaco2 = document.querySelector('.errormsgLName')
+
     var inputLastName = document.querySelector('#inputLastName')
     var lastName = inputLastName.value
     const msgErrorLastName = `<p style="color: red; margin: 0;text-align: right;">Last Name cannot be empty</p>`
     const labelLasteName = document.querySelector('.errormsgLName')
 
-    if(lastName == ""){
+    if(lastName == "" && espaco2.childNodes.length == 0){
         let elementLastName = document.createElement('label')
         elementLastName.innerHTML = msgErrorLastName
         labelLasteName.appendChild(elementLastName)
@@ -31,12 +35,14 @@ document.querySelector('#submit').addEventListener('click', function() {
 })
 
 document.querySelector('#submit').addEventListener('click', function(){
+var espaco3 = document.querySelector('.errormsgEmail')
+
     var inputEmail = document.querySelector('#inputEmail')
     var email = inputEmail.value
     const msgErrorEmail = `<p style="color: red; margin: 0;text-align: right;">Looks like this is not an email</p>`
     const labelEmail = document.querySelector('.errormsgEmail')
 
-    if(email == ""){
+    if(email == "" && espaco3.childNodes.length == 0){
         let elementEmail = document.createElement('label')
         elementEmail.innerHTML = msgErrorEmail
         labelEmail.appendChild(elementEmail)
